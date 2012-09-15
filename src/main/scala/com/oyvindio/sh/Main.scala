191104e162894d1dd40ec14f989ac6be75b49f66
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2012-09-15 12:48:19 +0200

    Actually remove references to Echo..

--- src/main/scala/com/oyvindio/sh/Main.scala
+++ src/main/scala/com/oyvindio/sh/Main.scala
@@ -1,14 +1,13 @@
 package com.oyvindio.sh
 
 import akka.actor.{Props, ActorSystem}
-import com.oyvindio.sh.modules.{MongoLogger, Echo}
+import com.oyvindio.sh.modules.MongoLogger
 
 object Main extends App {
   val system = ActorSystem("scalahugs")
   val bot = new Scalahugs(system)
   val botActor = system.actorOf(Props(new BotActor(bot)), "bot")
 
-  system.eventStream.subscribe(system.actorOf(Props(new Echo(botActor.path)), "echo"), classOf[PrivMsg])
   system.eventStream.subscribe(system.actorOf(Props(new MongoLogger(botActor.path)), "mongoLogger"), classOf[PrivMsg])
 
   bot.start()
