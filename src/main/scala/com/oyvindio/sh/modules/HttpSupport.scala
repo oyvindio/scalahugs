abfa4ca6e449f73523f11b7834df493e7d74c627
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2012-11-04 19:48:15 +0100

    Make http requests follow redirects by default

--- src/main/scala/com/oyvindio/sh/modules/HttpSupport.scala
+++ src/main/scala/com/oyvindio/sh/modules/HttpSupport.scala
@@ -3,6 +3,7 @@ package com.oyvindio.sh.modules
 import dispatch.url
 
 trait HttpSupport {
-  def request(uri: String) = url(uri).setHeader("User-Agent",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:12.0) Gecko/20100101 Firefox/12.0")
+  def request(uri: String) = url(uri)
+    .setHeader("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:12.0) Gecko/20100101 Firefox/12.0")
+    .setFollowRedirects(true)
 }
