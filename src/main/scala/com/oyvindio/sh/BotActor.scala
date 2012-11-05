114b6794c92d90965685f43127a6ba391f97a7b5
Øyvind Ingebrigtsen Øvergaard <oyvind.overgaard@gmail.com>
2012-11-05 23:27:01 +0100

    Exclude sender and bot nick in pingall

--- src/main/scala/com/oyvindio/sh/BotActor.scala
+++ src/main/scala/com/oyvindio/sh/BotActor.scala
@@ -1,7 +1,7 @@
 package com.oyvindio.sh
 
 import akka.actor.{ActorLogging, Actor}
-import events.bot.{Users, Op, BotMsg}
+import events.bot.{Nick, Users, Op, BotMsg}
 import scala.collection.JavaConversions._
 
 class BotActor(bot: Scalahugs) extends Actor with ActorLogging {
@@ -12,9 +12,8 @@ class BotActor(bot: Scalahugs) extends Actor with ActorLogging {
       val user = bot.getUsers(op.channel).find(_.getNick.equals(op.nick))
       if (user.isDefined && !user.get.isOp) bot.op(op.channel, op.nick)
     }
-    case users: Users => {
-      sender ! bot.getUsers(users.channel).toList
-    }
+    case users: Users => sender ! bot.getUsers(users.channel).toList
+    case nick: Nick => sender ! bot.getNick
     case _ => log.warning("Got unexpected message!")
   }
 }
--- src/main/scala/com/oyvindio/sh/modules/PingAll.scala
+++ src/main/scala/com/oyvindio/sh/modules/PingAll.scala
@@ -3,10 +3,12 @@ package com.oyvindio.sh.modules
 import akka.actor.ActorPath
 import com.oyvindio.sh.events.Trigger
 import com.oyvindio.sh.events.bot.Users
+import com.oyvindio.sh.events.bot.Nick
 import akka.pattern.AskSupport
 import org.jibble.pircbot.User
 import akka.util.Timeout
 import akka.util.duration._
+import akka.dispatch.Await
 
 
 class PingAll(botpath: ActorPath) extends AbstractScalahugsActor(botpath) with AskSupport {
@@ -15,9 +17,16 @@ class PingAll(botpath: ActorPath) extends AbstractScalahugsActor(botpath) with A
   protected def receive = {
     case trigger: Trigger if trigger.trigger == "pingall" => {
       (bot ? Users(trigger.channel)).mapTo[List[User]] onSuccess {
-        case users: List[User] => privMsg(trigger.channel,
-          "%s: %s is trying to reach you!".format(users.map(_.getNick).mkString(", "),
-                                                  trigger.nick))
+        case users: List[User] => {
+          val future = (bot ? new Nick).mapTo[String] onSuccess {
+            case botNick: String => botNick
+          }
+          val excluded = List(Await.result(future, 50 millis), trigger.nick)
+          val nicks = users.map(_.getNick).filterNot(excluded contains _)
+          privMsg(trigger.channel,
+            "%s: %s is trying to reach you!".format(nicks.mkString(", "),
+              trigger.nick))
+        }
       }
     }
   }
